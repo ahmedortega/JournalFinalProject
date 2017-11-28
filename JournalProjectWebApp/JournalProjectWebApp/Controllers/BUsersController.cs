a5a58992514b0d2205f99391cdf79ebfcfe84ace
DESKTOP-4KP52D6\Ahmed Ortega <ahmedsniper2020@outlook.com>
2017-11-28 17:56:04 +0200

    Controllers

---
+++ JournalProjectWebApp/JournalProjectWebApp/Controllers/BUsersController.cs
@@ -0,0 +1,96 @@
+﻿using JournalDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JournalProjectWebApp.Controllers
+{
+    public class BUsersController : ApiController
+    {
+        public IEnumerable<BUser> GetBUsers()
+        {
+            using (JournalEntities _entities = new JournalEntities())
+            {
+                return _entities.BUsers.ToList();
+            }
+        }
+        public BUser GetBUserById(int id)
+        {
+            using (JournalEntities _entities = new JournalEntities())
+            {
+                var busers = _entities.BUsers.FirstOrDefault(c => c.Id == id);
+                return busers;
+            }
+        }
+        public HttpResponseMessage PostBUser(BUser buser)
+        {
+            using (JournalEntities _entities = new JournalEntities())
+            {
+                try
+                {
+                    buser.UserType = 2;
+                    _entities.BUsers.Add(buser);
+                    _entities.SaveChanges();
+                    var msg = Request.CreateResponse(HttpStatusCode.Created, buser);
+                    msg.Headers.Location = new Uri(Request.RequestUri + "/" + buser.Id);
+                    return msg;
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+        }
+        public HttpResponseMessage DeleteBUser(int id)
+        {
+            using (JournalEntities _entities = new JournalEntities())
+            {
+                try
+                {
+                    var buser = _entities.BUsers.FirstOrDefault(c => c.Id == id);
+                    _entities.BUsers.Remove(buser);
+                    _entities.SaveChanges();
+                    var msg = Request.CreateResponse(HttpStatusCode.Created, buser);
+                    msg.Headers.Location = new Uri(Request.RequestUri + "/" + buser.Id + "is Deleted");
+                    return msg;
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+        }
+        public HttpResponseMessage PutBUser(int id, BUser buser)
+        {
+            using (JournalEntities _entities = new JournalEntities())
+            {
+                try
+                {
+                    var myBuser = _entities.BUsers.FirstOrDefault(c => c.Id == id);
+                    if (myBuser == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, " no Business User with this ID");
+                    }
+                    else
+                    {
+                        myBuser.Fname = buser.Fname;
+                        myBuser.Lname = buser.Lname;
+                        myBuser.Username = buser.Username;
+                        myBuser.Password = buser.Password;
+                        myBuser.Email = buser.Email;
+                        myBuser.UserType = 2;
+                        _entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, " The Business User is Updated ");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+        }
+    }
+}
---
+++ JournalProjectWebApp/JournalProjectWebApp/Controllers/VUsersController.cs
@@ -0,0 +1,98 @@
+﻿using JournalDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JournalProjectWebApp.Controllers
+{
+    public class VUsersController : ApiController
+    {
+        public IEnumerable<VUser> GetVUsers()
+        {
+            using (JournalEntities _entities = new JournalEntities())
+            {
+                return _entities.VUsers.ToList();
+            }
+        }
+        public VUser GetVUserById(int id)
+        {
+            using (JournalEntities _entities = new JournalEntities())
+            {
+                var vuser = _entities.VUsers.FirstOrDefault(c => c.Id == id);
+                return vuser;
+            }
+        }
+        public HttpResponseMessage PostVUser(VUser vuser)
+        {
+            using (JournalEntities _entities = new JournalEntities())
+            {
+                try
+                {
+                    vuser.UserType = 1;
+                    _entities.VUsers.Add(vuser);
+                    _entities.SaveChanges();
+                    var msg = Request.CreateResponse(HttpStatusCode.Created, vuser);
+                    msg.Headers.Location = new Uri(Request.RequestUri + "/" + vuser.Id);
+                    return msg;
+                }
+                catch(Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+        }
+        public HttpResponseMessage DeleteVUser(int id)
+        {
+            using (JournalEntities _entities = new JournalEntities())
+            {
+                try
+                {
+                    var vuser = _entities.VUsers.FirstOrDefault(c => c.Id == id);
+                    _entities.VUsers.Remove(vuser);
+                    _entities.SaveChanges();
+                    var msg = Request.CreateResponse(HttpStatusCode.Created, vuser);
+                    msg.Headers.Location = new Uri(Request.RequestUri + "/" + vuser.Id + "is Deleted");
+                    return msg;
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+        }
+        public HttpResponseMessage PutVUser(int id, VUser vuser)
+        {
+            using (JournalEntities _entities = new JournalEntities())
+            {
+                try
+                {
+                    var myVuser = _entities.VUsers.FirstOrDefault(c => c.Id == id);
+                    if (myVuser == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, " no Visitor User with this ID");
+                    }
+                    else
+                    {
+                        myVuser.Fname = vuser.Fname;
+                        myVuser.Lname = vuser.Lname;
+                        myVuser.Username = vuser.Username;
+                        myVuser.Password = vuser.Password;
+                        myVuser.Email = vuser.Email;
+                        myVuser.UserType = 1;
+                        _entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, " The Visitor User is Updated ");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+        }
+
+
+    }
+}
